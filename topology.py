61c76ce9e5ff1bb011205e7ecaf2f0e77e0e3076
Zefkilis2002 <110336606+Zefkilis2002@users.noreply.github.com>
2024-09-02 15:49:53 +0200

    Update topology.py
    Documentazione

--- topology.py
+++ topology.py
@@ -1,3 +1,4 @@
+# Importazioni necessarie dalle librerie di Mininet e Python
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import OVSKernelSwitch, RemoteController
@@ -7,32 +8,37 @@ from mininet.log import setLogLevel
 import subprocess
 import os
 
+# PARTE 1: Definizione della topologia della rete
 class SlicingTopo(Topo):
     def __init__(self):
         # Inizializza la topologia
-        super(SlicingTopo, self).__init__()  # In Python 3, è buona pratica usare super()
+        super(SlicingTopo, self).__init__()
 
         # Configurazione degli host e dei link
-        host_config = dict(inNamespace=True)
-        link_config = dict()
+        host_config = dict(inNamespace=True) # Gli host verranno creati in spazi dei nomi (namespace)
+        link_config = dict() # Configurazione base per i link
 
         # Creazione dello switch centrale
-        core_switch = self.addSwitch('s1', dpid="0000000000000001")
+        core_switch = self.addSwitch('s1', dpid="0000000000000001") # Creazione dello switch centrale con Nome e ID
 
-        # Creazione degli switch di distribuzione
+        # Creazione degli switch di distribuzione (con un for)
         for i in range(4):
-            dist_switch = self.addSwitch("s%d" % (i + 2), dpid="%016x" % (i + 2))
+            dist_switch = self.addSwitch("s%d" % (i + 2), dpid="%016x" % (i + 2)) 
             self.addLink(core_switch, dist_switch, **link_config)  # Collegamento tra core switch e distribution switch
 
             # Creazione di 2 host per ogni switch di distribuzione
             for j in range(2):
-                host = self.addHost("h%d" % (2*i + j + 1), **host_config)
+                host = self.addHost("h%d" % (2*i + j + 1), **host_config) # Creazione dell'host con un nome univoco
                 self.addLink(dist_switch, host, **link_config)  # Collegamento tra distribution switch e host
+
 
 topos = {"networkslicingtopo": (lambda: SlicingTopo())}
 
+
+# PARTE 2: Main principale
 if __name__ == "__main__":
-    topo = SlicingTopo()
+    topo = SlicingTopo() # Crea un'istanza della topologia
+    # Configura la rete Mininet con la topologia definita
     net = Mininet(
         topo=topo,
         switch=OVSKernelSwitch,
@@ -44,19 +50,21 @@ if __name__ == "__main__":
 
     setLogLevel("info")
 
+    # Configura un controller remoto (OpenFlow) con indirizzo IP e porta specificati
     controller = RemoteController("c0", ip="127.0.0.1", port=6633)
-    net.addController(controller)
-    net.build()
-    net.start()
+    net.addController(controller) # Aggiunge il controller alla rete
+    net.build()  # Costruisce la rete (creazione dei nodi, configurazione dei link, ecc.)
+    net.start() # Avvia la rete
 
-    # Attivazione del server UDP su slice 2 (se applicabile)
+    # Attivazione del server UDP sulla slice 2
     udp_servers = ["h3", "h4"]
 
+    # Itera su tutti gli host della rete
     for h in net.hosts:
-        if h.name in udp_servers:
-            h.cmd('iperf -s -u -p 5050 &')
+        if h.name in udp_servers:  # Se l'host è uno di quelli designati come server UDP
+            h.cmd('iperf -s -u -p 5050 &') # Avvia un server UDP usando iperf sulla porta 5050
 
-    # Path dello script
+    # Path dello script di attivazione delle slice
     script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "slice_script","attiva_slicing.sh")
 
     # Esegue lo script bash e cattura l'output
